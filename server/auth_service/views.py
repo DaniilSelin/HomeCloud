70d22502fbd36b4e9489bf2482d43bfec70fc90d
daniilselin <dan.selin2004@mail.ru>
2024-08-08 21:01:21 +0700

    логига токенов почти закончена

--- server/auth_service/views.py
+++ server/auth_service/views.py
@@ -17,10 +17,111 @@ AnswerFromAuthService = {
 
 class AuthService:
     @staticmethod
-    def generate_token():
-        # Логика генерации токена
+    def generate_token(data):
         import uuid
-        return str(uuid.uuid4())
+
+        db = next(get_bd())
+
+        try:
+            data = request.get_json()
+
+            expiry_days = data.get('expiry', 30)
+
+            token = RegistrationToken(
+                token=str(uuid.uuid4()),
+                expiry_date=datetime.datetime.utcnow() + datetime.timedelta(days=expiry_days)
+            )
+
+            db.add(token)
+            db.commit()
+
+            return jsonify({'message': 'Token created successfully'}), 201
+
+        except SQLAlchemyError as e:
+            db.rollback()
+            return jsonify({'error': str(e), 'message': 'Failed to create token'}), 500
+
+        except Exception as e:
+            return jsonify({'error': str(e), 'message': 'An unexpected error occurred'}), 500
+
+    @staticmethod
+    def get_tokens():
+        db = next(get_bd())
+
+        try:
+            tokens = db.query(RegistrationToken).all()
+
+            # Преобразуем объекты в словари для JSON сериализации
+            tokens_list = [{
+                'token': t.token,
+                'created_at': t.created_at.isoformat(),
+                "expiry_date": t.expiry_date} for t in tokens]
+
+            return jsonify({
+                'message': 'Tokens retrieved successfully',
+                "data": tokens_list
+            }), 200
+
+        except SQLAlchemyError as e:
+            db.rollback()
+            return jsonify({'error': str(e), 'message': 'Failed to get tokens'}), 500
+
+        except Exception as e:
+            return jsonify({'error': str(e), 'message': 'An unexpected error occurred'}), 500
+
+    @staticmethod
+    def update_token_expiry(data):
+        db = next(get_bd())
+
+        try:
+            token = data.get("token", None)
+            expiry = data.get('expiry', 30)
+
+            if token is None:
+                return jsonify({'error': 'Token is required', 'message': 'Failed to update token expiry'}), 400
+
+            reqToken = db.query(
+                RegistrationToken).filter(
+                RegistrationToken.token == token).first()
+
+            if reqToken is None:
+                return jsonify({'error': 'Token not found', 'message': 'Failed to update token expiry'}), 404
+
+            reqToken.expiry_date = datetime.datetime.utcnow() + datetime.timedelta(days=expiry)
+            db.commit()
+
+            return jsonify({
+                'message': 'Token expiry update successfully',
+                "data": {'token': token, 'expiry_date': reqToken.expiry_date}
+            }), 201
+
+        except SQLAlchemyError as e:
+            db.rollback()
+            return jsonify({'error': str(e), 'message': 'Failed to update token expiry'}), 500
+
+        except Exception as e:
+            return jsonify({'error': str(e), 'message': 'An unexpected error occurred'}), 500
+
+    @staticmethod
+    def delete_expired_token():
+        db = next(get_bd())
+
+        try:
+            db.delete(
+                RegistrationToken).filter(
+                RegistrationToken.expiry_date > datetime.datetime.utcnow())
+
+            return jsonify({
+                'message': 'All expired Token delete',
+                "data": {}
+            }), 201
+
+        except SQLAlchemyError as e:
+            db.rollback()
+            return jsonify({'error': str(e), 'message': 'Failed to delete expired token'}), 500
+
+        except Exception as e:
+            return jsonify({'error': str(e), 'message': 'An unexpected error occurred'}), 500
 
     @staticmethod
     def register_user(data):
@@ -38,57 +139,24 @@ auth_blueprint = Blueprint('auth', __name__)
 @auth_blueprint.route("/auth/generate_token", methods=["POST"])
 def generate_token():
     """Создает новый токен"""
-    db = next(get_bd())
-
-    try:
-        data = request.get_json()
-
-        expiry_days = data.get('expiry_days', 30)
-
-        token = RegistrationToken(
-            token=AuthService.generate_token(),
-            expiry_date=datetime.datetime.utcnow() + datetime.timedelta(days=expiry_days)
-        )
-
-        db.add(token)
-        db.commit()
-
-        return jsonify({'message': 'Token created successfully'}), 201
-
-    except SQLAlchemyError as e:
-        db.rollback()
-        return jsonify({'error': str(e), 'message': 'Failed to create token'}), 500
-
-    except Exception as e:
-        return jsonify({'error': str(e), 'message': 'An unexpected error occurred'}), 500
+    return AuthService.generate_token(data=request.get_json())
 
 
 @auth_blueprint.route("/auth/get_tokens", methods=["GET"])
 def get_tokens():
     """ Возвращает все токены"""
-    db = next(get_bd())
+    return AuthService.get_tokens()
 
-    try:
-        tokens = db.query(RegistrationToken).all()
 
-        # Преобразуем объекты в словари для JSON сериализации
-        tokens_list = [{
-            'regToken_id': t.regToken_id,
-            'token': t.token,
-            'created_at': t.created_at.isoformat(),
-            "expiry_date": t.expiry_date} for t in tokens]
+@auth_blueprint.route("/auth/update_token", methods=["POST"])
+def update_token_expiry():
+    """ Обновляет время действия токена на переданное количество дней"""
+    return AuthService.update_token_expiry(data=request.get_json())
 
-        return jsonify({
-            'message': 'Tokens retrieved successfully',
-            "data": tokens_list
-        }), 200
 
-    except SQLAlchemyError as e:
-        db.rollback()
-        return jsonify({'error': str(e), 'message': 'Failed to get tokens'}), 500
-
-    except Exception as e:
-        return jsonify({'error': str(e), 'message': 'An unexpected error occurred'}), 500
+@auth_blueprint.route("/auth/delete_expired_token")
+def delete_expired_token():
+    return AuthService.delete_expired_token()
 
 
 @auth_blueprint.route('/auth/register', methods=['POST'])
@@ -96,7 +164,6 @@ def register():
     data = request.get_json()
     print("/auth/register", data)
     return
-
 
 @auth_blueprint.route("/auth/login", methods=["POST"])
 def login():
