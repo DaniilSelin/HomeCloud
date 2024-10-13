b409d18dc89092a018badc74f119212dfefb8b1e
daniilselin <dan.selin2004@mail.ru>
2024-10-13 18:05:19 +0700

    НОВЫЙ НОВЫЙ ПОДХОД

--- server/auth_service/test/check_connect.py
+++ server/auth_service/test/check_connect.py
@@ -6,18 +6,23 @@ BASE_URL_2 = 'http://localhost:5000'
 
 BASE_URL_3 = 'http://127.0.0.1:5000'
 
+print("start test connect")
 
 def ping():
     data = []
     for BASE_URL in [BASE_URL_1, BASE_URL_2, BASE_URL_3]:
         try:
             response = requests.post(BASE_URL + '/auth/login', json={
-            'name': 'homeuser',
-            'password': 'changeme'
+                'name': 'homeuser',
+                'password': 'changeme'
             })
             data.append(response.json())
+            print("Connect successful")
         except Exception as e:
+            print("Connect retry?")
             print(e)
     print(data)
 
 ping()
+
+print("end test connect")
