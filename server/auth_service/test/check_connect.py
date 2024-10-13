2113613a6ddc8bc0a4710e99b9de5382fe8e1a50
daniilselin <dan.selin2004@mail.ru>
2024-10-13 17:12:40 +0700

    НОВЫЙ НОВЫЙ ПОДХОД

---
+++ server/auth_service/test/check_connect.py
@@ -0,0 +1,23 @@
+import requests
+
+BASE_URL_1 = 'http://auth_service:5000'
+
+BASE_URL_2 = 'http://localhost:5000'
+
+BASE_URL_3 = 'http://127.0.0.1:5000'
+
+
+def ping():
+    data = []
+    for BASE_URL in [BASE_URL_1, BASE_URL_2, BASE_URL_3]:
+        try:
+            response = requests.post(BASE_URL + '/auth/login', json={
+            'name': 'homeuser',
+            'password': 'changeme'
+            })
+            data.append(response.json())
+        except Exception as e:
+            print(e)
+    print(data)
+
+ping()
